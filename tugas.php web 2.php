4541988f8a837119cf450ee1e61f18b130eb1877
hafizauza <80506715+hafizauza@users.noreply.github.com>
2022-03-03 18:56:07 -0800

    Add files via upload

---
+++ tugas.php web 2.php
@@ -0,0 +1,14 @@
+<?php
+
+  $nim = "20090124";
+  $nama = "HAFIZAUZA";
+  $kelas = "4A";
+    echo "My Biodata <br>
+    NIM   : $nim <br>
+    Nama  : $nama <br>
+    Kelas : $kelas <br>";
+
+ print("Semoga setelah mengenal kita bisa menyayangi sebab ada pepatah mengatakan 
+        - Tak kenal maka tak sayang -");
+  
+?>
